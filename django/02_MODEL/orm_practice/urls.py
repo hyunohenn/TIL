9cb4223d54b0fcb8deefda608b8dca9af60e3849
hyunohenn <hyun.ohenn@gmail.com>
2021-03-11 23:49:43 +0900

    0311 TIL

--- django/02_MODEL/orm_practice/urls.py
+++ django/02_MODEL/orm_practice/urls.py
@@ -7,8 +7,20 @@ urlpatterns = [
     path('', views.index, name='index' ),
     # practice/1/ => 1번 학생 정보 보여주기 HTML(단일 조회)
     path('<int:pk>/', views.detail, name='detail'),
+
+    # 기존의 데이터를 다루는 것이 아니라 이렇게만 해도 됨
     # practice/new/ => 새로운 데이터 입력용 HTML(생성 준비단계)
     path('new/', views.new, name='new'),
     # practice/create/ => 사용자 입쳑력 데이터 처리
     path('create/', views.create, name='create'),
+
+    
+    # 일종의 레거시이므로 특정한 데이터를 지정해주어야 한다
+    # practice/1/edit/ => 기존의 데이터를 수정할 HTML
+    path('<int:pk>/edit/', views.edit, name='edit'), 
+    # practice/1/update/ => 사용자 입력 데이터 처리
+    path('<int:pk>/update/', views.update, name='update'),
+    
+    # practice/1/delete/ => 데이터 삭제
+    path('<int:pk>/delete/', views.delete, name='delete'),
 ]
--- django/02_MODEL/orm_practice/views.py
+++ django/02_MODEL/orm_practice/views.py
@@ -1,31 +1,62 @@
 from django.shortcuts import render, redirect
 from . models import Student
 
+# Creat (생성)
+## HTML 제공
+## 바로 아래 구간에서는 HTML을 주는 것에 그친다
+def new(request):
+    return render(request, 'orm_practice/new.html')
+
+## 실제 저장
+def create(request):
+    if request.method == 'POST':
+        student = Student()
+        student.name = request.POST.get('name')
+        student.age = request.POST.get('age')
+        student.major = request.POST.get('major')
+        student.hobby = request.POST.get('hobby')
+        student.save()
+        # redirect(RAW URL/ urls.py 의 name)
+        return redirect('detail', pk=student.pk)  # 넘겨줄때 인자가 필요하므로
+    return redirect('new')
+
 # Retrieve / Read (조회)
+## 여러개 조회
 def index(request):
     students = Student.objects.all()
     context = {'students': students}
     return render(request,'orm_practice/index.html', context)
 
-
+## 단일 조회
 def detail(request, pk):
     student = Student.objects.get(id=pk)
     context= {'student': student}
     return render(request,'orm_practice/detail.html', context)
 
 
-# Creat (생성)
-# 바로 아래 구간에서는 HTML을 주는 것에 그친다
-def new(request):
-    return render(request, 'orm_practice/new.html')
+# update
+## 수정용 HTML 제공
+def edit(request, pk):
+    student = Student.objects.get(pk=pk)  # 기존의 내용을 보여주어야 함
+    context = {'student': student}
+    return render(request, 'orm_practice/edit.html', context)
 
+## 실제 수정
+def update(request, pk):
+    if request.method == 'POST':
+        student = Student.objects.get(pk=pk)
+        student.name = request.POST.get('name')
+        student.age = request.POST.get('age')
+        student.major = request.POST.get('major')
+        student.hobby = request.POST.get('hobby')
+        student.save()
+        return redirect('detail', pk=student.pk)
+    return redirect('edit', pk=pk)
 
-def create(request):
-    student = Student()
-    student.name = request.GET.get('name')
-    student.age = request.GET.get('age')
-    student.major = request.GET.get('major')
-    student.hobby = request.GET.get('hobby')
-    student.save()
-    # redirect(RAW URL/ urls.py 의 name)
-    return redirect('detail', pk=student.pk)  # 넘겨줄때 인자가 필요하므로
+# Delete
+def delete(request, pk):
+    if request.method == 'POST':
+        student = Student.objects.get(pk=pk)
+        student.delete()
+        return redirect('index')
+    return redirect('detail', pk=pk)
