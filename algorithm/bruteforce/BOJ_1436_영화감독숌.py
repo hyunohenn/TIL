b7878ec6aed0bedcede66e7c9e30b63d35275637
hyunohenn <hyun.ohenn@gmail.com>
2021-07-20 23:55:51 +0900

    0720 TIL algorithm BOJ_1436

---
+++ algorithm/bruteforce/BOJ_1436_영화감독숌.py
@@ -0,0 +1,15 @@
+# 영화감독 숌
+
+## 숌 주제에 반지의제왕을 뛰어넘으려 들어?
+
+N = int(input())
+cnt = 0
+initial = 666
+ans = 0
+while cnt != N:
+    if '666' in str(initial):
+        cnt += 1
+        ans = initial
+    initial += 1
+
+print(ans)
